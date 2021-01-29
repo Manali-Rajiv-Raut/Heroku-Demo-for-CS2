d475103fd7fdbef33b87a6b6f90419ef8a810582
Manali <73314716+Manali-Rajiv-Raut@users.noreply.github.com>
2021-01-30 00:59:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,8 +81,8 @@ def predict():
     elif "\n" in text :
         text2.append(text.split("\n"))
     
-    #sentence = [ line for line in text2]    
-    sen_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(text2))
+    sentence = [ line for line in text2]    
+    sen_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(sentence))
     predicted_cat = label_encoder_acbsa.inverse_transform(np.argmax(acbsa_model.predict(sen_tokenized), axis=-1))       
     predicted_polarity =label_encoder_sentiment.inverse_transform(np.argmax(sentiment_model.predict(sen_tokenized), axis=-1))
     result = dfc.create_result_dataframe(predicted_cat,predicted_polarity)

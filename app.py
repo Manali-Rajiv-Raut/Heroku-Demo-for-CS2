34e5e9bcce44ed274c696a8c055bc33011219d1a
Manali <73314716+Manali-Rajiv-Raut@users.noreply.github.com>
2021-01-30 00:14:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def predict():
         text2 = text.split(',')
     """
     sentence = [ line for line in text2]    
-    sen_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(sentence))
+    sen_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(text))
     predicted_cat = label_encoder_acbsa.inverse_transform(np.argmax(acbsa_model.predict(sen_tokenized), axis=-1))       
     predicted_polarity =label_encoder_sentiment.inverse_transform(np.argmax(sentiment_model.predict(sen_tokenized), axis=-1))
     result = dfc.create_result_dataframe(predicted_cat,predicted_polarity)

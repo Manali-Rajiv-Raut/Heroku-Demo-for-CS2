a2738ee2680a80449405ce62ccfe46d554386039
Manali <73314716+Manali-Rajiv-Raut@users.noreply.github.com>
2021-01-29 16:29:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from tensorflow.python.keras.layers import Dropout
 from flask import Flask, request, jsonify, render_template
 from flask_table import Table, Col
 from flask import Markup
-from flask import jsonify
 
 app = Flask(__name__)
 path_acbsa = "saved_model/acbsa_model.h5"
@@ -27,55 +26,55 @@ path_le_acbsa = 'saved_model/label_encoder_acbsa'
 path_le_sentiment = 'saved_model/label_encoder_sentiment'
 #print(IPython.__version__)
 
+# loading the tokenizer
 with open(path_tokenizer, 'rb') as f:               
     tokenizer = pickle.load(f)
 
+#Loading Lable encoder for aspect based sentiemnt analysis
 with open(path_le_acbsa, 'rb') as f:               
     label_encoder_acbsa = pickle.load(f)
 
+#Loading Lable encoder for sentiemnt analysis
 with open(path_le_sentiment, 'rb') as f:               
     label_encoder_sentiment = pickle.load(f) 
     
+# aspect catagory based analysis
+acbsa_model = Sequential(name="acbsa")                                                   
+acbsa_model.add(Dense(512, input_shape=(6000,), activation='relu'))
+acbsa_model.add((Dense(256, activation='relu')))
+acbsa_model.add((Dropout(0.3)))
+acbsa_model.add((Dense(128, activation='relu')))
+acbsa_model.add(Dense(5, activation='softmax'))
+acbsa_model.load_weights(path_acbsa)
 
-def acbsa_model_creation():
-    acbsa_model = Sequential(name="acbsa")                                                   
-    acbsa_model.add(Dense(512, input_shape=(6000,), activation='relu'))
-    acbsa_model.add((Dense(256, activation='relu')))
-    acbsa_model.add((Dropout(0.3)))
-    acbsa_model.add((Dense(128, activation='relu')))
-    acbsa_model.add(Dense(5, activation='softmax'))
-    acbsa_model.load_weights(path_acbsa)
-    return acbsa_model
+# sentiment analysis
+sentiment_model = Sequential(name="sentiment")
+sentiment_model.add(Dense(512, input_shape=(6000,), activation='relu'))
+sentiment_model.add((Dense(256, activation='relu')))
+sentiment_model.add((Dropout(0.3)))
+sentiment_model.add((Dense(128, activation='relu')))
+sentiment_model.add(Dense(4, activation='softmax'))
+sentiment_model.load_weights(path_sentiment)
 
-def sentiment_model_creation():
-    sentiment_model = Sequential(name="sentiment")
-    sentiment_model.add(Dense(512, input_shape=(6000,), activation='relu'))
-    sentiment_model.add((Dense(256, activation='relu')))
-    sentiment_model.add((Dropout(0.3)))
-    sentiment_model.add((Dense(128, activation='relu')))
-    sentiment_model.add(Dense(4, activation='softmax'))
-    sentiment_model.load_weights(path_sentiment)
-    return sentiment_model 
-  
-    
+#loading the preprocessing class
+dp = Data_Preprocessing()
+#loading the darframe creation class
+dfc = DataFrame_Creation()
+     
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    acbsa_model = acbsa_model_creation()
-    sentiment_model = sentiment_model_creation()
-    dp = Data_Preprocessing()
-    dfc = DataFrame_Creation()
     
     # store the given text in a variable
     text = request.form.get("text")
-    text2 = text.split('\n')
-    sentence = [ line for line in text2]
-    print(sentence)
+    #text2 = text.split('\n')
+    #sentence = [ line for line in text2]
+    #print(sentence)
     
-    sen_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(sentence))
+    sen_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(text))
     predicted_cat = label_encoder_acbsa.inverse_transform(np.argmax(acbsa_model.predict(sen_tokenized), axis=-1))       
     predicted_polarity =label_encoder_sentiment.inverse_transform(np.argmax(sentiment_model.predict(sen_tokenized), axis=-1))
     result = dfc.create_result_dataframe(predicted_cat,predicted_polarity)
@@ -85,13 +84,6 @@ def predict():
     
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run() 
+    app.run(debug=True) 
     
     
-    
-    
-    
-    
-    
-            

d4800743c09d38f1b261838c3d835a5ae8c147b2
Manali <73314716+Manali-Rajiv-Raut@users.noreply.github.com>
2021-01-30 11:34:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,26 +41,35 @@ with open(path_le_sentiment, 'rb') as f:
     label_encoder_sentiment = pickle.load(f) 
     
 # aspect catagory based analysis
-acbsa_model = Sequential(name="acbsa")                                                   
-acbsa_model.add(Dense(512, input_shape=(6000,), activation='relu'))
-acbsa_model.add((Dense(256, activation='relu')))
-acbsa_model.add((Dropout(0.3)))
-acbsa_model.add((Dense(128, activation='relu')))
-acbsa_model.add(Dense(5, activation='softmax'))
+def acbsa_model_creation():
+    acbsa_model = Sequential(name="acbsa")                                                   
+    acbsa_model.add(Dense(512, input_shape=(6000,), activation='relu'))
+    acbsa_model.add((Dense(256, activation='relu')))
+    acbsa_model.add((Dropout(0.3)))
+    acbsa_model.add((Dense(128, activation='relu')))
+    acbsa_model.add(Dense(5, activation='softmax'))
+    acbsa_model.load_weights(path_acbsa)
+    return acbsa_model
 
 # sentiment analysis
-sentiment_model = Sequential(name="sentiment")
-sentiment_model.add(Dense(512, input_shape=(6000,), activation='relu'))
-sentiment_model.add((Dense(256, activation='relu')))
-sentiment_model.add((Dropout(0.3)))
-sentiment_model.add((Dense(128, activation='relu')))
-sentiment_model.add(Dense(4, activation='softmax'))
-
-
+def sentiment_model_creation():
+    sentiment_model = Sequential(name="sentiment")
+    sentiment_model.add(Dense(512, input_shape=(6000,), activation='relu'))
+    sentiment_model.add((Dense(256, activation='relu')))
+    sentiment_model.add((Dropout(0.3)))
+    sentiment_model.add((Dense(128, activation='relu')))
+    sentiment_model.add(Dense(4, activation='softmax'))
+    sentiment_model.load_weights(path_sentiment)
+    return sentiment_model
+    
 #loading the preprocessing class
 dp = Data_Preprocessing()
 #loading the darframe creation class
 dfc = DataFrame_Creation()
+# loading acbsa model
+acbsa_model = acbsa_model_creation()
+# loading sentiment model
+sentiment_model = sentiment_model_creation()
      
 @app.route('/')
 def home():
@@ -68,9 +77,6 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    acbsa_model.load_weights(path_acbsa)
-    sentiment_model.load_weights(path_sentiment)
-    
     # store the given text in a variable
     text = request.form.get("text")
     sentence = nltk.tokenize.sent_tokenize(text)   
@@ -79,7 +85,6 @@ def predict():
     predicted_polarity =label_encoder_sentiment.inverse_transform(np.argmax(sentiment_model.predict(sen_tokenized), axis=-1))
     result = dfc.create_result_dataframe(predicted_cat,predicted_polarity)
     html = result.to_html() 
-    
     return render_template('index.html', prediction_text= Markup(html))
     
 
